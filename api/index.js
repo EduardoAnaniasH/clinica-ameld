ef20322fce1f46d9eed2c505e86614877cd0e9d1
Eduardo Ananias <edholanda588@gmail.com>
2025-10-21 02:29:54 -0300

    Corrige localização do db.json para a Vercel

--- api/index.js
+++ api/index.js
@@ -2,18 +2,19 @@ const jsonServer = require('json-server');
 const path = require('path');
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '..', 'public', 'db.json'));
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
-// Re-escreve as rotas para remover o prefixo /api
+server.use(middlewares);
+
+// Re-escreve as rotas para remover o prefixo /api que a Vercel usa
 server.use((req, res, next) => {
     if (req.url.startsWith('/api')) {
-        req.url = req.url.substring(4); // Remove /api
+        req.url = req.url.substring(4);
     }
     next();
 });
 
-server.use(middlewares);
 server.use(router);
 
 module.exports = server;
