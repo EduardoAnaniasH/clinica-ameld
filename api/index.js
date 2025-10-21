a6b9764a863c286c70ecacc4af5681f2b23dc079
Eduardo Ananias <edholanda588@gmail.com>
2025-10-21 02:18:30 -0300

    Ajusta o manuseamento de rotas da API na Vercel

--- api/index.js
+++ api/index.js
@@ -2,17 +2,18 @@ const jsonServer = require('json-server');
 const path = require('path');
 
 const server = jsonServer.create();
-// O caminho para o db.json precisa ser absoluto no ambiente da Vercel
-const router = jsonServer.router(path.resolve(process.cwd(), 'public', 'db.json'));
+const router = jsonServer.router(path.join(__dirname, '..', 'public', 'db.json'));
 const middlewares = jsonServer.defaults();
 
+// Re-escreve as rotas para remover o prefixo /api
+server.use((req, res, next) => {
+    if (req.url.startsWith('/api')) {
+        req.url = req.url.substring(4); // Remove /api
+    }
+    next();
+});
+
 server.use(middlewares);
-
-// Reescreve as rotas para remover o prefixo /api
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-}));
-
 server.use(router);
 
 module.exports = server;
